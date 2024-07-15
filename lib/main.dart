06b4548df156867a8d777443f01dc0748123e706
Kauan <cvitorcordeiro@gmail.com>
2024-07-14 21:32:17 -0300

    textfields

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,28 @@ class _HomeState extends State<Home> {
           IconButton(onPressed: () {}, icon: Icon(Icons.refresh))
         ],
       ),
+      backgroundColor: Colors.white,
+      body: const Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Peso (KG)",
+              labelStyle: TextStyle(color: Colors.green, fontSize: 25.0),
+            ),
+            textAlign: TextAlign.center,
+          ),TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Altura (CM)",
+              labelStyle: TextStyle(color: Colors.green, fontSize: 25.0),
+            ),
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
     );
   }
 }
