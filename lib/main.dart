642ac5c91547236d81d0214a8f604fb00e225794
Kauan <cvitorcordeiro@gmail.com>
2024-07-14 22:11:58 -0300

    calculate

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,36 @@ class _HomeState extends State<Home> {
   String _infoText = "Informe seus dados!";
 
   void _resetFields(){
+    setState(() {
       weightController.text = "";
       heightController.text = "";
-      _infoText = "Resetado!";
+      _infoText = "Informe seus dados!";
+    });
+  }
+
+  void _calculate(){
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+      if(imc < 18.6){
+        _infoText = "Abaixo do peso (${imc.toStringAsPrecision(4)})";
+      }else if(imc >= 18.6 && imc < 24.9){
+        _infoText = "Peso Ideal (${imc.toStringAsPrecision(4)})";
+      }else if(imc >= 24.9 && imc < 29.9){
+        _infoText = "Levemente acima do peso (${imc.toStringAsPrecision(4)})";
+
+      }else if(imc >= 29.9 && imc < 34.9){
+        _infoText = "Obesidade Grau 1 (${imc.toStringAsPrecision(4)})";
+
+      }else if(imc >= 34.9 && imc < 39.9){
+        _infoText = "Obesidade Grau 2 (${imc.toStringAsPrecision(4)})";
+
+      }else if (imc >= 40){
+        _infoText = "Obesidade Grau 3 (${imc.toStringAsPrecision(4)})";
+
+      }
+    });
   }
 
   @override
@@ -69,7 +96,7 @@ class _HomeState extends State<Home> {
               child: Container(
                 height: 50.0,
                 child: ElevatedButton(
-                  onPressed: add,
+                  onPressed: _calculate,
                   child: Text(
                     "Calcular",
                     style: TextStyle(color: Colors.white, fontSize: 25),
