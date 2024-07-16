2fc734372ee90e93f38f8bacfe66656512876db1
Kauan <cvitorcordeiro@gmail.com>
2024-07-15 21:11:07 -0300

    reset error

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class _HomeState extends State<Home> {
       weightController.text = "";
       heightController.text = "";
       _infoText = "Informe seus dados!";
+      _formKey = GlobalKey<FormState>();
     });
   }
 
